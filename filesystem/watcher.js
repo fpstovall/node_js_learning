0045822e60d2c4279e51461705d5fb55d04c0668
Rick Stovall <fpstovall@gmail.com>
2018-06-10 19:42:36 -0400

    Example one... works.

---
+++ filesystem/watcher.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+'use strict';
+
+const fs = require('fs');
+
+fs.watch('target.txt', () => console.log('File changed!'));
+
+console.log('Now watching target.txt for changes....');
+
