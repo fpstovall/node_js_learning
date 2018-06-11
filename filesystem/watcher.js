b5336b784aa9a71140a87486971c1db1242c4051
Rick Stovall <fpstovall@gmail.com>
2018-06-10 20:09:24 -0400

    file watcher with spawn.

--- filesystem/watcher.js
+++ filesystem/watcher.js
@@ -3,13 +3,17 @@
 'use strict';
 
 const fs = require('fs');
+const spawn = require('child_process').spawn;
 
 const filename = process.argv[2];
 if (!filename) {
     throw Error('A file to watch must be specified');
 }
 
-fs.watch(filename, () => console.log(`File ${filename} has changed!`));
+fs.watch(filename, () => {
+    const ls = spawn('ls',['-lh',filename]);
+    ls.stdout.pipe(process.stdout);
+});
 
 console.log(`Now watching ${filename} for changes....`);
 
